c8de8d34bb9a19d3c2cd58c17a07b49e63db6e01
LAHI-RU <lahiiru.dananjaya@gmail.com>
2024-11-24 15:00:31 +0530

    update

---
+++ client/src/Post.js
@@ -0,0 +1,26 @@
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="image">
+        <img src="https://assets.toptal.io/images?url=https%3A%2F%2Fbs-uploads.toptal.io%2Fblackfish-uploads%2Fcomponents%2Fblog_post_page%2F8343864%2Fcover_image%2Fretina_300x120%2FBoosting-Developer-Productivity-with-AI_blog%2520%25281%2529-2b23c1ffbbd8e1288de7af9204c584a7.png"></img>
+      </div>
+
+      <div className="texts">
+        <h2>
+          Increase Developer Productivity With Generative AI: Tips From Leading
+          Software Engineers
+        </h2>
+        <p className="info">
+          <a className="author">Lahiru Bandara</a>
+          <time>2024-11-21 14:51</time>
+        </p>
+        <p className="summary">
+          Generative AI is revolutionizing how software developers write code.
+          In this article, three Toptal developers share how they’re using Gen
+          AI in their daily work and offer actionable advice for others who want
+          to utilize this nascent technology.
+        </p>
+      </div>
+    </div>
+  );
+}
