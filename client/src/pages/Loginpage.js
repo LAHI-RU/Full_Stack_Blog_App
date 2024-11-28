ccc0fbf597689e7b4c7ca0411245bde6ad550ccc
LAHI-RU <lahiiru.dananjaya@gmail.com>
2024-11-28 08:28:51 +0530

    update

--- client/src/pages/Loginpage.js
+++ client/src/pages/Loginpage.js
@@ -1,6 +1,7 @@
 export default function Loginpage() {
   return (
     <form className="login">
+      <h1>Login</h1>
       <input type="text" placeholder="username" />
       <input type="password" placeholder="password" />
       <button>Login</button>
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,7 +1,7 @@
 export default function Registerpage() {
   return (
     <form className="register">
-      <h1>Login</h1>
+      <h1>Register</h1>
       <input type="text" placeholder="username" />
       <input type="password" placeholder="password" />
       <button>Register</button>
