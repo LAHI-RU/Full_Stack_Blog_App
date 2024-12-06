a0f6e53f5c1c115a9807b6286cec22220e712608
LAHI-RU <lahiiru.dananjaya@gmail.com>
2024-12-06 23:57:12 +0530

    update

--- api/index.js
+++ api/index.js
@@ -1,16 +1,2 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.post("/register", (req, res) => {
-  const { username, password } = req.body;
-  res.json({ requestdata: { username, password } });
-});
-
-app.listen(4000);
-
 //connection string
 //password
