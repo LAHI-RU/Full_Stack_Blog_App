eacd46efc0771f8b3e502de7abbfab634cd264eb
LAHI-RU <lahiiru.dananjaya@gmail.com>
2024-12-08 15:28:25 +0530

    update

--- api/index.js
+++ api/index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const User = require("./models/User");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/register", (req, res) => {
+mongoose.connect(
+  "mongodb+srv://Mern-Blog:7yJdz0gNTpjjkSrG@cluster0.ucbln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
+
+app.post("/register", async (req, res) => {
   const { username, password } = req.body;
+  const userDoc = await User.create({ username, password });
   res.json({ requestdata: { username, password } });
 });
 
 app.listen(4000);
 
-//mongodb+srv://Mern-Blog:7yJdz0gNTpjjkSrG@cluster0.ucbln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 //7yJdz0gNTpjjkSrG
